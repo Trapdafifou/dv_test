6638c1f395a79a89b02bac811aacacd1b687a469
TMarsalMutum <t.marsal@mutum.fr>
2016-11-10 04:58:17 +0100

    Vuejs CRUD V_0.0.2

---
+++ README.md
@@ -0,0 +1,15 @@
+#Hello DV
+
+Voici mon CRUD d'annuaire en Vuejs. Les données sont stockées en localStorage. Vous pouvez ajouter le numéro de Donald (personne ne le verra)
+
+C'est avec plaisir que je travaillerais avec vous et Maurice (the DV's doge).
+Sachez que ma détermination est sans faille et que malgré certaines difficultées par manque mon manque d'expérience, je ne lache rien.
+
+Il m'aura fallut 2 heures de plus pour terminer mon annuaire. Je pense qu'il m'aurait plus fallut 2h pour reprendre les bases de Vuejs.
+Quoi qu'il en soit j'ai mon annuaire fonctionnel après un peu de travail et une heure de coucher un peu tardive.
+
+Quoi qu'il advienne, je vous remercie de l'attention portée pour mon entretien et votre sourire.
+Si la réponse est non, je serais preneur de conseils avec plaisir, si vous en avez le temps.
+
+Bonne nuit.
+
--- src/main.js
+++ src/main.js
@@ -1,70 +1,103 @@
 var app = new Vue(({
     el: '#app',
     data: {
-        entries: []
+        lastName: '',
+        firstName: '',
+        email: '',
+        phone: '',
+        entries: [],
+        editedItem: undefined,
+        cachedItem:{}
     },
     created: function () {
-        this.getData();
+            this.getData();
+
     },
 
-    getData: function () {
-        localStorage.getItem('entries')
-    },
+    methods: {
+        getData: function () {
+            var localData = localStorage.getItem('entries');
+            return this.entries = JSON.parse(localData);
 
-    postData: function (item) {
-        localStorage.setItem('entries', JSON.stringify(item));
-    },
+        },
+        postData: function () {
+            var jsonData = JSON.stringify(this.entries);
+            localStorage.setItem('entries', jsonData);
+            console.log(localStorage.getItem('entries'))
+        },
+        deleteData: function (index) {
+            this.entries.splice(index, 1);
+            var jsonData = JSON.stringify(this.entries);
+            localStorage.setItem('entries', jsonData);
+            console.log(localStorage.getItem('entries'))
+        },
+        updateMode: function (item) {
+            this.editedItem = item;
 
-    getFormValue: function () {
-        var lastName = document.querySelector('#last-name'),
-            firstName = document.querySelector('#first-name'),
-            email = document.querySelector('#email'),
-            phone = document.querySelector('#phone');
+            this.lastName = this.entries[item].lastName;
+            this.firstName = this.entries[item].firstName;
+            this.email = this.entries[item].email;
+            this.phone = this.entries[item].phone;
+            return;
+        },
+        updateData: function (item) {
 
-        this.errorCheck(lastName, firstName, email, phone);
-    },
+            this.cachedItem = this.entries[item];
 
-    errorCheck: function (lastName, firstName, email, phone) {
-        if (!lastName || typeof lastName != "string") {
-            alert('put a string in the input last-name');
-            return;
-        }
-        if (!firstName || typeof firstName != "string") {
-            alert('put a string in the input first-name');
-            return;
-        }
-        if (!email || typeof email != "string") {
-            alert('put a string in the input email');
-            return;
-        }
-        if (!phone || typeof phone != "string") {
-            alert('put a string in the input phone');
-            return;
-        }
-        else {
-            var entrie = {
-                lastName: '',
-                firstName: '',
-                email: '',
-                phone: ''
+            this.entries[item] = {
+                lastName: this.lastName,
+                firstName: this.firstName,
+                email: this.email,
+                phone: this.phone,
             };
 
-            this.createEntrie(entrie);
-        }
-    },
+            this.postData()
 
-    createEntrie: function (item) {
-        this.data.entries.push(item);
+            this.lastName = '';
+            this.firstName = '';
+            this.email = '';
+            this.phone = '';
+        },
 
-        this.postData(item)
+        errorCheck: function (lastName, firstName, email, phone) {
+            if (!lastName || typeof lastName != "string") {
+                alert('put a string in the input last-name');
+                return;
+            }
+            if (!firstName || typeof firstName != "string") {
+                alert('put a string in the input first-name');
+                return;
+            }
+            if (!email || typeof email != "string") {
+                alert('put a string in the input email');
+                return;
+            }
+            if (!phone || typeof phone != "string") {
+                alert('put a string in the input phone');
+                return;
+            }
+            else {
+                var entrie = {
+                    lastName: lastName,
+                    firstName: firstName,
+                    email: email,
+                    phone: phone
+                };
+
+                this.createEntrie(entrie);
+            }
+        },
+
+        createEntrie: function (item) {
+            this.entries.push(item);
+
+            this.postData(item)
+
+            this.lastName = '';
+            this.firstName = '';
+            this.email = '';
+            this.phone = '';
+        },
+
     }
 }));
-
-var btn = document.querySelector('.btn');
-
-btn.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log('yoo');
-
-    app.getFormValue();
-})
